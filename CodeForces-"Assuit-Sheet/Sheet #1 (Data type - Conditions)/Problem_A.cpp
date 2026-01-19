ca337da95e10443adc8ab85719e7c949a2e4d046
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-01-19 23:21:06 +0200

    Add Problem A solution to the Assuit Sheet

---
+++ CodeForces-"Assuit-Sheet/Sheet #1 (Data type - Conditions)/Problem_A.cpp
@@ -0,0 +1,14 @@
+/* ************************************************************
+ * 🚀 Solved by Eng/ Mostafa Tarek 
+ * 📁 Part of: Problem Solving Journey (Assiut Sheet)
+ * ************************************************************ */
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+    getline(cin,name);
+    cout<<"Hello, "<<name;
+    return 0;
+}

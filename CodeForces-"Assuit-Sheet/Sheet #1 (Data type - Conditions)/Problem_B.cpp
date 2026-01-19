1b8d03e4201ab55c0b25e213773843c0bffdc8f1
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-01-19 23:40:43 +0200

    Add Problem B solution with input/output handling

---
+++ CodeForces-"Assuit-Sheet/Sheet #1 (Data type - Conditions)/Problem_B.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************
+ * 🚀 Solved by Eng/ Mostafa Tarek
+ * 📁 Part of: Problem Solving Journey (Assiut Sheet)
+ * ************************************************************ */
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int var1;
+  long long var2;
+  char var3;
+  float var4;
+  double var5;
+  cin >> var1 >> var2 >> var3 >> var4 >> var5 ;
+  cout << var1 << endl << var2 << endl << var3 << endl << var4 << endl << var5 << endl ;
+  return 0;
+}

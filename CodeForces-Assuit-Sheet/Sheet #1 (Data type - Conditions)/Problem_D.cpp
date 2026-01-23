3e78be0ef6a43d17ef48f3fcbfb9ab98f52049dd
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-01-23 22:53:27 +0200

    Add Problem D solution for calculating differences
    Implemented a program to calculate the difference between two products.

---
+++ CodeForces-Assuit-Sheet/Sheet #1 (Data type - Conditions)/Problem_D.cpp
@@ -0,0 +1,13 @@
+/* ************************************************************
+ * 🚀 Solved by Eng/ Mostafa Tarek 
+ * 📁 Part of: Problem Solving Journey (Assiut Sheet)
+ * ************************************************************ */
+#include <iostream>
+using namespace std;
+int main()
+ {
+  	long long a,b,c,d;
+        cin>>a>>b>>c>>d;
+  		cout<<"Difference = "<<((a*b)-(c*d))<<endl;
+  return 0;
+ }

fcb6d8391dc29dbb89dd7e45579a84ebf828c54b
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-01-23 22:57:33 +0200

    Add Problem E solution for area of a circle

---
+++ CodeForces-Assuit-Sheet/Sheet #1 (Data type - Conditions)/Problem_E.cpp
@@ -0,0 +1,15 @@
+/* ************************************************************
+ * 🚀 Solved by Eng/ Mostafa Tarek 
+ * 📁 Part of: Problem Solving Journey (Assiut Sheet)
+ * ************************************************************ */
+#include <iostream>
+#include <iomanip>
+using namespace std;
+const double pi =3.141592653;
+int main()
+ {
+  	double r;
+	cin>>r;
+	cout<<setprecision(9)<<fixed<<pi*(r*r)<<endl;
+  return 0;
+ }

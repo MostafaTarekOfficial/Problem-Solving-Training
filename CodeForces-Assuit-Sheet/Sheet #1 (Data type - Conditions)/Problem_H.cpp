738e8fcca5e11af8a2f6045b1181414d50a3e867
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-01-27 11:57:24 +0200

    Add Problem_H.cpp for floor, ceil, and round calculations

---
+++ CodeForces-Assuit-Sheet/Sheet #1 (Data type - Conditions)/Problem_H.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************
+ * 🚀 Solved by Eng/ Mostafa Tarek 
+ * 📁 Part of: Problem Solving Journey (Assiut Sheet)
+ * ************************************************************ */
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+ {
+  	double A,B;
+  	cin>>A>>B;
+  	double result = A/B;
+  	cout << "floor " << A << " / " << B << " = " << (long long)floor(result) << endl;
+    cout << "ceil " << A << " / " << B << " = " << (long long)ceil(result) << endl;
+    cout << "round " << A << " / " << B << " = " << (long long)round(result) << endl;
+  return 0;
+ }
